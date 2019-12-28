e465fea4b46ba926a0ffa12370c3531c6a16e77d
Erica Schimmel <ericaschimmel01@gmail.com>
2019-12-28 03:57:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,21 @@
 class Dog
+<<<<<<< HEAD
   def initialize(name, breed = "Mutt")
     @name = name
     @breed = breed
   end
+=======
+  def intialize(name, breed)
+    @name = name
+    @breed = breed
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed
+    @breed
+  end
+>>>>>>> 274faac0aac76ff0165800b37fc81aec3ad97922
 end
